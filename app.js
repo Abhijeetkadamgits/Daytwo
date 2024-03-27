69f972888c7adf813d81d68b2f16b5e77dba5433
Abhijitkadamgit <94745660+Abhijeetkadamgits@users.noreply.github.com>
2024-03-27 19:45:48 +0530

    second-last-change

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var ele = gsap.timeline();
 
-ele.from("#logo,#first-h1", {
+ele.from("#first-h1", {
   y:-100,
   duration :1.5,
   opacity :0.1,
   stagger : 0.2
+});
+gsap.from("#logo",{
+  duration :2,
+  opacity :0.1,
+  scale : 0.2
 });
 gsap.from("#left-img",{
   duration: 1.5, 
@@ -13,4 +18,9 @@ gsap.from("#left-img",{
 gsap.from("#right-img",{
   y:-100,
   duration:1.5
+});
+gsap.from("#btm",{
+  y:100,
+  duration: 1.5,
+  stagger : 0.2
 });
